8fff653ffa43c0d1e581ac347323e95b4ecb8a1c
Putra Ganda Dewata <putraganda.dewata@gmail.com>
2023-10-17 01:33:35 +0700

    Added Basic Maps Implementation

---
+++ app/src/main/java/com/putragandad/learnmaps/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.putragandad.learnmaps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        replaceFragment(MapsFragment())
+    }
+
+    private fun replaceFragment (fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container, fragment)
+        fragmentTransaction.commit()
+    }
+}
