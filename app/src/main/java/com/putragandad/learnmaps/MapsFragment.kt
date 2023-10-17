7af412a5a140fc19d60cad1542319b4d8d51ba24
Putra Ganda Dewata <putraganda.dewata@gmail.com>
2023-10-17 22:45:52 +0700

    Added BottomSheetDialog for each marker onClick
    - Using Google built-in googleMap.setOnMarkerClickListener
    - Made a functions called showBottomSheet :
      1. To inflate BottomSheetDialog
      2. To set value from Marker (title, snippet, latitude, longitude) to bottomsheet's TextView
      3. To start intent to open Google Maps based on marker value of latitude and longitude
    
    2. Made a functions called openGoogleMaps :
       1. To start intent to open Google Maps based on latitude and longitude value, using Uri and Intent.ACTION_VIEW
    
    3. Created Basic BottomSheetDialog with essential info and button for redirect to Google Maps

--- app/src/main/java/com/putragandad/learnmaps/MapsFragment.kt
+++ app/src/main/java/com/putragandad/learnmaps/MapsFragment.kt
@@ -1,13 +1,17 @@
 package com.putragandad.learnmaps
 
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Location
+import android.net.Uri
 import androidx.fragment.app.Fragment
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -22,6 +26,9 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import java.util.function.DoubleBinaryOperator
 
 class MapsFragment : Fragment() {
 
@@ -46,10 +53,11 @@ class MapsFragment : Fragment() {
             googleMap.setOnMarkerClickListener {
                 Toast.makeText(context, "Clicked! " + it.snippet + " " + it.title , Toast.LENGTH_SHORT).show()
                 it.showInfoWindow()
+
+                showBottomSheet(it.title, it.snippet, it.position.latitude, it.position.longitude)
                 true
             }
         }
-
     }
 
     override fun onCreateView(
@@ -70,6 +78,36 @@ class MapsFragment : Fragment() {
         locationArrayList!!.add(MapsData(-2.961700781169533, 104.74892014785055, "Location 1", "Jalan Taqwa 1"))
         locationArrayList!!.add(MapsData(-2.9831295839366834, 104.74686021144464, "Location 2", "Jalan Taqwa 2"))
         locationArrayList!!.add(MapsData(-2.9814152950450232, 104.77484101429141, "Location 3", "Jalan Taqwa 3"))
+    }
+
+    private fun showBottomSheet(placeName: String?, placeAddress: String?, latitude: Double?, longitude: Double?) {
+        var view = layoutInflater.inflate(R.layout.bottomsheet_maps, null)
+        var dialog = BottomSheetDialog(requireContext())
+
+        val tvPlaceName : TextView = view.findViewById(R.id.tv_placeName)
+        val tvPlaceAddress : TextView = view.findViewById(R.id.tv_placeAddress)
+        val tvLatitude: TextView = view.findViewById(R.id.tv_latitude)
+        val tvLongitude: TextView = view.findViewById(R.id.tv_longitude)
+        val btnGoogleMaps: Button = view.findViewById(R.id.btn_gmaps)
+
+        tvPlaceName.text = placeName
+        tvPlaceAddress.text = placeAddress
+        tvLatitude.setText(latitude.toString())
+        tvLongitude.setText(longitude.toString())
+
+        btnGoogleMaps.setOnClickListener() {
+            openGoogleMaps(latitude, longitude)
+        }
+
+        dialog.setContentView(view)
+        dialog.show()
+
+    }
+
+    private fun openGoogleMaps(latitude: Double?, longitude: Double?) {
+        var mapUri: Uri = Uri.parse("https://maps.google.com/maps/search/" + latitude + "," + longitude)
+        val intent = Intent(Intent.ACTION_VIEW, mapUri)
+        startActivity(intent)
     }
 
 }
